d3f01f85d0d594382ef78f0d92dadd904ea85d6c
Barrett Helzel <barrett.p.helzel@gmail.com>
2020-04-30 12:02:31 -0700

    touch up question form

--- src/components/question.jsx
+++ src/components/question.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-export const Question = ({ text, category, answer, dummyAnswers}) => {
-    return(
-        <div>
-            <p>{text}</p>
-            <p>{category}</p>
-            <p>{answer}</p>
-            <p>{dummyAnswers}</p>
-        </div>
-    )
-}
+// export const Question = ({ text, category, answer, dummyAnswers}) => {
+//     return(
+//         <div>
+//             <p>{text}</p>
+//             <p>{category}</p>
+//             <p>{answer}</p>
+//             <p>{dummyAnswers}</p>
+//         </div>
+//     )
+// }
